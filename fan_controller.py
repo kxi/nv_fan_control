ffaadfe40adb41d3a77ed83606facf2dfeb34291
Kai Xi <kaixi.work@gmail.com>
2018-05-20 01:16:54 +1000

    Fix Small Index Bug

--- fan_controller.py
+++ fan_controller.py
@@ -42,7 +42,7 @@ def main():
             new_fan_speed = 100
 
         if abs(recent_fan_speed - new_fan_speed) <= 3:
-            LOGGER.info("[NO_ACT]: GPU #{} Target Fan Speed = {}, Recent Fan Speed = {}".format(new_fan_speed, recent_fan_speed))
+            LOGGER.info("[NO_ACT]: GPU #{} Target Fan Speed = {}, Recent Fan Speed = {}".format(i, new_fan_speed, recent_fan_speed))
         else:
             process = subprocess.Popen("DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a \"[fan-{}]/GPUTargetFanSpeed={}\"".format(i, new_fan_speed), stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
             output, error = process.communicate()
