dc65e15f9da5df5073d4b94e5a020c4bd8a2b30d
Kai Xi <kaixi.work@gmail.com>
2018-05-20 00:58:37 +1000

    Add No Action if Recent Meets Target

--- fan_controller.py
+++ fan_controller.py
@@ -19,27 +19,36 @@ def main():
     # while True:
     process = subprocess.Popen("nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader", stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
     output = process.communicate()[0]
+    temperature_list = output.splitlines()
 
-    for i, line in enumerate(output.splitlines(),0):
-        gputemp = int(line.decode())
-        LOGGER.info("[STATUS]: GPU #{} Temperature = {}".format(i, gputemp))
-
-        if gputemp <= 40:
-            newfanspeed = 35
-
-        if gputemp > 40 and gputemp < 75:
-            newfanspeed = gputemp + delta
-            newfanspeed = min(newfanspeed, 100)
-
-        if gputemp >= 75:
-            newfanspeed = 100
+    process = subprocess.Popen("nvidia-smi --query-gpu=fan.speed --format=csv,noheader", stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
+    output = process.communicate()[0]
+    fan_speed_list = output.splitlines()
 
 
-        process = subprocess.Popen("DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a \"[fan-{}]/GPUTargetFanSpeed={}\"".format(i, newfanspeed), stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
-        output, error = process.communicate()
-        if output:
-            LOGGER.info("[ACTION]: {}".format(output.decode().strip()))
-        if error:
-            LOGGER.critical("[ERROR]: {}".format(error.decode().strip()))
+    for i, line in enumerate(temperature_list,0):
+        gpu_temp = int(line.decode())
+        recent_fan_speed = int(fan_speed_list[i].decode().strip().strip("%"))
+        LOGGER.info("[STATUS]: GPU #{} Temperature = {}. Recent Fan Speed = {}".format(i, gpu_temp, recent_fan_speed))
+
+        if gpu_temp <= 40:
+            new_fan_speed = 35
+
+        if gpu_temp > 40 and gpu_temp < 75:
+            new_fan_speed = gpu_temp + delta
+            new_fan_speed = min(new_fan_speed, 100)
+
+        if gpu_temp >= 75:
+            new_fan_speed = 100
+
+        if abs(recent_fan_speed - new_fan_speed) <= 3:
+            LOGGER.info("[NO_ACT]: Target Fan Speed = {}, Recent Fan Speed = {}".format(new_fan_speed, recent_fan_speed))
+        else:
+            process = subprocess.Popen("DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a \"[fan-{}]/GPUTargetFanSpeed={}\"".format(i, new_fan_speed), stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
+            output, error = process.communicate()
+            if output:
+                LOGGER.info("[ACTION]: {}".format(output.decode().strip()))
+            if error:
+                LOGGER.critical("[ERROR]: {}".format(error.decode().strip()))
 
 main()
